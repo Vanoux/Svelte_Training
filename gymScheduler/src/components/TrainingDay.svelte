56c1e22418173875fa8173891ebbad361978753d
Vanoux <vanessa.roux891@orange.fr>
2019-08-18 16:08:12 +0200

    Ajout component TrainingDaysDetails + Affichage dans component TrainingDay

--- gymScheduler/src/components/TrainingDay.svelte
+++ gymScheduler/src/components/TrainingDay.svelte
@@ -1,14 +1,16 @@
 <script>
+import TrainingDaysDetails from "./TrainingDaysDetails.svelte";
+
 
 //tableau d'objets de jours
 const nbOfDays = [
-    { id: 1, text: "One day", value: 1 },
-    { id: 2, text: "Two days", value: 2 },
-    { id: 3, text: "Three days", value: 3 },
-    { id: 4, text: "Four days", value: 4 },
-    { id: 5, text: "Five days", value: 5 },
-    { id: 6, text: "Six days", value: 6 },
-    { id: 7, text: "Seven days", value: 7}
+    { id: 1, text: "One day", value: [1] },
+    { id: 2, text: "Two days", value: [1, 2] },
+    { id: 3, text: "Three days", value: [1, 2, 3] },
+    { id: 4, text: "Four days", value: [1, 2, 3, 4] },
+    { id: 5, text: "Five days", value: [1, 2, 3, 4, 5] },
+    { id: 6, text: "Six days", value: [1, 2, 3, 4, 5, 6] },
+    { id: 7, text: "Seven days", value: [1, 2, 3, 4, 5, 6, 7]}
 ];
 //recupération de la selection de l'utilisateur (menu déroulant)
 let nbDaysTrainingPerWeek;
@@ -17,14 +19,24 @@ let nbDaysTrainingPerWeek;
 
 <style></style>
 
+<!-- Dropdown list -->
 <div>
     <form>
         <select bind:value={nbDaysTrainingPerWeek}>
-            <!-- itération sur la collection du tableau d'ojet + association à une clef unique -->
+            <!-- itération sur la collection du tableau d'ojet + association à une clef unique (day.id)-->
             {#each nbOfDays as day (day.id)} 
-                <option value={day}>{day.text}</option>
+                <option value={day}>{day.text} per week</option>
             {/each}
         </select>
     </form>
     {JSON.stringify(nbDaysTrainingPerWeek)}
 </div>
+
+{#if nbDaysTrainingPerWeek}
+<!-- Affichage du nb de jours d'entrainement du Component TrainingDaysDetails-->
+<div>
+    {#each nbDaysTrainingPerWeek.value as dayNumber, index}
+        <TrainingDaysDetails title={`day ${++index}`}></TrainingDaysDetails>
+    {/each}
+</div>
+{/if}
---
+++ gymScheduler/src/components/TrainingDaysDetails.svelte
@@ -0,0 +1,19 @@
+<script>
+    export let title;
+    let exercice = {
+        activity: "",
+        series: 4,
+        repetitions: 10
+    }
+
+    let exercices = [];
+
+</script>
+
+
+
+<style></style>
+
+<div>
+    <h2>{title}</h2>
+</div>
